3cb73ef51a26e314e3b9d25472b6414a22ec0822
Manoel Paulino <mpaulino.me@gmail.com>
2022-09-26 21:51:21 -0300

    up

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var configuration = app.Configuration;
+ProductRepository.Init(configuration);
 
 app.MapPost("/products", (Product product) => {
     ProductRepository.Add(product);
@@ -16,27 +18,32 @@ app.MapGet("/products/{code}", ([FromRoute] string code) => {
     return Results.NotFound();
 });
 
-app.MapPut("products", (Product product) => {
+app.MapPut("/products", (Product product) => {
     var productSaved = ProductRepository.GetBy(product.Code);
     productSaved.Name = product.Name;
     return Results.Ok();
 });
 
-app.MapDelete("products/{code}", ([FromRoute] string code) => {
+app.MapDelete("/products/{code}", ([FromRoute] string code) => {
     var productSaved = ProductRepository.GetBy(code);
     ProductRepository.Remove(productSaved); 
     return Results.Ok();
 });
 
+app.MapGet("/configuration/database", (IConfiguration configuration) => {
+    return Results.Ok($"{configuration["database:connection"]}/{configuration["database:port"]}");
+});
+
 app.Run();
 
 public static class ProductRepository {
-    public static List<Product> Products { get; set; }
+    public static List<Product> Products { get; set; } = Products = new List<Product>();
 
-    public static void Add(Product product) {
-        if(Products == null)
-            Products = new List<Product>();
-
+    public static void Init(IConfiguration configuration) {
+        var products = configuration.GetSection("Products").Get<List<Product>>();
+        Products = products;
+    }
+    public static void Add(Product product) {            
         Products.Add(product);
     }
 
